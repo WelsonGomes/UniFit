1461961645f63cb6f555c04f306359e36527f23e
Welson Gomes <welsondouglasgomes@gmail.com>
2024-09-10 16:56:26 -0300

    atualização

--- build/server.js
+++ build/server.js
@@ -5,10 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
+app.use((0, cors_1.default)({ origin: "*" }));
 const port = 3333;
-app.get('/', (res) => {
+app.get('/', (req, res) => {
     return res.status(200).json({ msg: `App Running on port ${port}` });
 });
 app.listen(port, () => {
